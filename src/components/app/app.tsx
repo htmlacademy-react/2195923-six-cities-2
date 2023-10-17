92454e2553267449c557bdb01c0d1a264163a53f
Dmitry Lutsishin <graf.luck@gmail.com>
2023-10-17 15:39:05 +0300

    описал маршруты в приложении

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,10 @@
 import MainPage from '../../pages/main-page/main-page';
+import LoginPage from '../../pages/login-page/login-page';
+import FavoritesPage from '../../pages/favorites-page/favorites-page';
+import NotFoundPage from '../../pages/not-found-page/not-found-page';
+import OfferPage from '../../pages/offer-page/offer-page';
+import PrivateRoute from '../private/private-route';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 type AppProps = {
   placesCount: number;
@@ -6,7 +12,15 @@ type AppProps = {
 
 function App({placesCount} : AppProps) : React.JSX.Element {
   return (
-    <MainPage placesCount={placesCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<MainPage placesCount={placesCount}/>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/favorites' element={<PrivateRoute><FavoritesPage /></PrivateRoute>} />
+        <Route path='/offer/:id' element={<OfferPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
