f8df0ac276253629eca8a882ac93d9786c6b9153
Dmitry Lutsishin <graf.luck@gmail.com>
2023-11-15 10:17:37 +0300

    реализовал выделение активного типа сортировки в списке

--- src/components/sorting/sorting.tsx
+++ src/components/sorting/sorting.tsx
@@ -34,6 +34,13 @@ function Sorting({onSortTypeClick, type} : SortingProps) {
     }
   };
 
+  if (type === SortingType.POPULAR) {
+    sortingRef.current?.querySelectorAll('.places__option').forEach((element) => {
+      element.classList.remove('places__option--active');
+    });
+    sortingRef.current?.querySelector('.places__option')?.classList.add('places__option--active');
+  }
+
   return (
     <form className="places__sorting" action="#" method="get" >
       <span className="places__sorting-caption">Sort by</span>
@@ -44,7 +51,7 @@ function Sorting({onSortTypeClick, type} : SortingProps) {
         </svg>
       </span>
       <ul className="places__options places__options--custom" ref={sortingRef} onClick={handleSortTypeClick}>
-        <li className="places__option places__option--active" tabIndex={0} data-type={SortingType.POPULAR.message}>Popular</li>
+        <li className="places__option" tabIndex={0} data-type={SortingType.POPULAR.message}>Popular</li>
         <li className="places__option" tabIndex={0} data-type={SortingType.PRICE_LOW_TO_HIGH.message}>Price: low to high</li>
         <li className="places__option" tabIndex={0} data-type={SortingType.PRICE_HIGH_TO_LOW.message}>Price: high to low</li>
         <li className="places__option" tabIndex={0} data-type={SortingType.TOP_RATED_FIRST.message}>Top rated first</li>
