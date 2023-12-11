4e695e7cf3d714240c4d995932de3db5bd999d2d
Dmitry Lutsishin <graf.luck@gmail.com>
2023-12-11 15:54:57 +0300

    выделил звезду рейтинга в отдельный компонент

--- src/components/rating/rating.tsx
+++ src/components/rating/rating.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { RatingLabel } from '../../const';
+import { RatingItem } from '../rating-item/rating-item';
 
 type RatingProps = {
   rating: number;
   onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
-function Rating({rating, onChange} : RatingProps) {
-  function generateRatingItem(label: [string, RatingLabel], index: number) {
-    return (
-      <React.Fragment key={5 - index}>
-        <input className="form__rating-input visually-hidden" name="rating" value={5 - index} id={`${5 - index}-stars`} type="radio" checked = {rating === (5 - index)}/>
-        <label htmlFor={`${5 - index}-stars`} className="reviews__rating-label form__rating-label" title={label[1]}>
-          <svg className="form__star-image" width="37" height="33">
-            <use xlinkHref="#icon-star"></use>
-          </svg>
-        </label>
-      </React.Fragment>
-    );
-  }
-
+function Rating({rating, onChange} : RatingProps) : React.JSX.Element {
   function generateRatingList() {
-    return Object.entries(RatingLabel).map((label, index) => generateRatingItem(label, index));
+    return Object.entries(RatingLabel).map((label, index) => RatingItem({label, index, rating}));
   }
 
   return (
