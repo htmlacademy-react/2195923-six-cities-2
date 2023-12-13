9bad02aafe29281378a9be45255e98f27d608813
Dmitry Lutsishin <graf.luck@gmail.com>
2023-12-13 15:46:57 +0300

    добавил заглушку при отображении главной страницы в случае отсутствия предложений

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -25,7 +25,7 @@ function MainPage() : React.JSX.Element {
   const [sortType, setSortType] = useState<TSortingType>('POPULAR');
 
   const offersByCity = useMemo(() => offers.filter((offer) => offer.city.name === cityName), [cityName, offers]);
-  const sortingOffers = useMemo(() => {
+  let sortingOffers = useMemo(() => {
     switch(sortType) {
       case 'POPULAR':
         return [...offersByCity];
@@ -73,21 +73,32 @@ function MainPage() : React.JSX.Element {
         <title>6 Cities</title>
       </Helmet>
       <Header isNavRequired isAuth={authStatus}/>
-      <main className="page__main page__main--index">
+      <main className={`page__main page__main--index ${sortingOffers.length ? '' : 'page__main--index-empty'} `} >
         <h1 className="visually-hidden">Cities</h1>
         <CitiesList currentCity={cityName} onCityClick={handleCityClick}/>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">{sortingOffers.length} places to stay in {cityName}</b>
-              <Sorting onSortTypeClick={handleSortingClick} type={sortType}/>
-              <PlaceCardList offers={sortingOffers} type={PlaceCardType.City} onMouseEnter={handlePlaceCardMouseEnter} onMouseLeave={handlePlaceCardMouseLeave} />
-            </section>
-            <div className="cities__right-section">
-              <Map cityName={cityName} offers={sortingOffers} type={'cities'}/>
+          {sortingOffers.length ?
+            <div className="cities__places-container container">
+              <section className="cities__places places">
+                <h2 className="visually-hidden">Places</h2>
+                <b className="places__found">{sortingOffers.length} places to stay in {cityName}</b>
+                <Sorting onSortTypeClick={handleSortingClick} type={sortType}/>
+                <PlaceCardList offers={sortingOffers} type={PlaceCardType.City} onMouseEnter={handlePlaceCardMouseEnter} onMouseLeave={handlePlaceCardMouseLeave} />
+              </section>
+              <div className="cities__right-section">
+                <Map cityName={cityName} offers={sortingOffers} type={'cities'}/>
+              </div>
             </div>
-          </div>
+            :
+            <div className="cities__places-container cities__places-container--empty container">
+              <section className="cities__no-places">
+                <div className="cities__status-wrapper tabs__content">
+                  <b className="cities__status">No places to stay available</b>
+                  <p className="cities__status-description">We could not find any property available at the moment in {cityName}</p>
+                </div>
+              </section>
+              <div className="cities__right-section"></div>
+            </div>}
         </div>
       </main>
     </div>
@@ -95,4 +106,3 @@ function MainPage() : React.JSX.Element {
 }
 
 export default MainPage;
-
