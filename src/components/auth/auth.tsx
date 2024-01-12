e6c32d41f674b3c6a91c450ff3b71488c6eff250
Dmitry Lutsishin <graf.luck@gmail.com>
2024-01-12 10:18:47 +0300

    реализовал тестирование компонента Auth

---
+++ src/components/auth/auth.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import { loginAction } from '../../store/actions/api-actions';
+import { AuthData } from '../../types/auth-data';
+
+function Auth() {
+  const PASSWORD_PATTERN = /(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]{2,}/;
+  const EMAIL_PATTERN = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  const dispatch = useAppDispatch();
+  const [password, setPassword] = useState<string>();
+  const [email, setEmail] = useState<string>();
+
+  const onFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    if (email && password) {
+      const authorizationData: AuthData = {
+        email: email,
+        password: password
+      };
+      dispatch(loginAction(authorizationData));
+    }
+  };
+
+  function isValidData() {
+    if (email && password && password.match(PASSWORD_PATTERN) && email.match(EMAIL_PATTERN)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  return (
+    <section className="login">
+      <h1 className="login__title">Sign in</h1>
+      <form className="login__form form" action="#" method="post" onSubmit={onFormSubmit}>
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">E-mail</label>
+          <input className="login__input form__input" type="email" name="email" placeholder="Email" required onChange={(evt) => setEmail(evt.target.value)} data-testid="emailElement"/>
+        </div>
+        <div className="login__input-wrapper form__input-wrapper">
+          <label className="visually-hidden">Password</label>
+          <input className="login__input form__input" type="password" name="password" placeholder="Password" required onChange={(evt) => setPassword(evt.target.value)} data-testid="passwordElement"/>
+        </div>
+        <button className="login__submit form__submit button" type="submit" disabled={isValidData()}>Sign in</button>
+      </form>
+    </section>
+  );
+}
+
+export default Auth;
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -2,45 +2,17 @@ import { Link, Navigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Header from '../../components/header/header';
 import { AppRoute } from '../../app-route';
-import { loginAction } from '../../store/actions/api-actions';
-import { AuthData } from '../../types/auth-data';
 import { useAppSelector } from '../../hooks/use-app-selector';
 import { AuthorizationStatus } from '../../const';
 import { getAuthorizationStatus } from '../../store/user-process/user-process.selectors';
-import { useState } from 'react';
-import { useAppDispatch } from '../../hooks/use-app-dispatch';
+import Auth from '../../components/auth/auth';
 
 function LoginPage() : React.JSX.Element {
-  const PASSWORD_PATTERN = /(?=.*[0-9])(?=.*[a-zA-Z])[0-9a-zA-Z]{2,}/;
-  const EMAIL_PATTERN = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-
   const authorizationStatus = useAppSelector(getAuthorizationStatus);
-  const dispatch = useAppDispatch();
-  const [password, setPassword] = useState<string>();
-  const [email, setEmail] = useState<string>();
+
 
   if (authorizationStatus === AuthorizationStatus.Auth) {
     return <Navigate to={AppRoute.Main} />;
-  }
-
-  const onFormSubmit = (evt: React.FormEvent<HTMLFormElement>) => {
-    evt.preventDefault();
-    if (email && password) {
-      const authorizationData: AuthData = {
-        email: email,
-        password: password
-      };
-      dispatch(loginAction(authorizationData));
-    }
-  };
-
-
-  function isValidData() {
-    if (email && password && password.match(PASSWORD_PATTERN) && email.match(EMAIL_PATTERN)) {
-      return false;
-    } else {
-      return true;
-    }
   }
 
   return (
@@ -51,20 +23,7 @@ function LoginPage() : React.JSX.Element {
       <Header isNavRequired={false}/>
       <main className="page__main page__main--login">
         <div className="page__login-container container">
-          <section className="login">
-            <h1 className="login__title">Sign in</h1>
-            <form className="login__form form" action="#" method="post" onSubmit={onFormSubmit}>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">E-mail</label>
-                <input className="login__input form__input" type="email" name="email" placeholder="Email" required onChange={(evt) => setEmail(evt.target.value)}/>
-              </div>
-              <div className="login__input-wrapper form__input-wrapper">
-                <label className="visually-hidden">Password</label>
-                <input className="login__input form__input" type="password" name="password" placeholder="Password" required onChange={(evt) => setPassword(evt.target.value)}/>
-              </div>
-              <button className="login__submit form__submit button" type="submit" disabled={isValidData()}>Sign in</button>
-            </form>
-          </section>
+          <Auth />
           <section className="locations locations--login locations--current">
             <div className="locations__item">
               <Link className="locations__item-link" to={AppRoute.Main}>
