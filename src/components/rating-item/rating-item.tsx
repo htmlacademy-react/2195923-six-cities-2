3f6220a9e2db61ad6f7773db763c00dcd13be0b7
Dmitry Lutsishin <graf.luck@gmail.com>
2024-01-22 16:06:40 +0300

    вынес магические числа в константы

--- src/components/rating-item/rating-item.tsx
+++ src/components/rating-item/rating-item.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { RatingLabel } from '../../const';
+import { MAX_RATING, RatingLabel } from '../../const';
 
 type RatingItemProps = {
   rating: number;
@@ -10,9 +10,9 @@ type RatingItemProps = {
 
 export function RatingItem({index, label, rating, onChange} : RatingItemProps) : React.JSX.Element {
   return (
-    <React.Fragment key={5 - index}>
-      <input className="form__rating-input visually-hidden" name="rating" value={5 - index} id={`${5 - index}-stars`} type="radio" checked = {rating === (5 - index)} onChange={onChange}/>
-      <label htmlFor={`${5 - index}-stars`} className="reviews__rating-label form__rating-label" title={label[1]}>
+    <React.Fragment key={MAX_RATING - index}>
+      <input className="form__rating-input visually-hidden" name="rating" value={MAX_RATING - index} id={`${MAX_RATING - index}-stars`} type="radio" checked = {rating === (MAX_RATING - index)} onChange={onChange}/>
+      <label htmlFor={`${MAX_RATING - index}-stars`} className="reviews__rating-label form__rating-label" title={label[1]}>
         <svg className="form__star-image" width="37" height="33">
           <use xlinkHref="#icon-star"></use>
         </svg>
